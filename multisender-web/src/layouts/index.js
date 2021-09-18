4e59976bc4a99b068eacfe9c86abe96891e16764
lolieatapple <lolieatapple@gmail.com>
2021-09-18 14:47:10 +0800

    update

--- multisender-web/src/layouts/index.js
+++ multisender-web/src/layouts/index.js
@@ -177,7 +177,7 @@ function BasicLayout(props) {
       </Head>
       <H1>Welcome to MultiSender</H1>
       <H2>This supports sending native coin and tokens from wallet to multiple addresses.</H2>
-      <H3>Network supported: Wanchain Mainnet/Testnet, BSC Mainnet/Testnet, Heco Mainnet/Testnet</H3>
+      <H3>Network supported: Wanchain Mainnet/Testnet, BSC Mainnet/Testnet, Heco Mainnet/Testnet, Moonriver</H3>
       <Body>
         <Text>Input or select token address:</Text>
         <span>
