9a5afd62ae5209ddfc07540d397bef6516e6d1c5
lolieatapple <lolieatapple@gmail.com>
2022-07-04 15:43:20 +0800

    remove testnet text

--- multisender-web/src/layouts/index.js
+++ multisender-web/src/layouts/index.js
@@ -179,7 +179,7 @@ function BasicLayout(props) {
       </Head>
       <H1>Welcome to MultiSender</H1>
       <H2>This supports sending native coin and tokens from wallet to multiple addresses.</H2>
-      <H3>Network supported: Wanchain Mainnet/Testnet, BSC Mainnet/Testnet, Heco Mainnet/Testnet, Moonriver, Avalanche</H3>
+      <H3>Network supported: Wanchain, BSC, Heco, Moonriver, Avalanche</H3>
       <Body>
         <Text>Input or select token address:</Text>
         <span>
